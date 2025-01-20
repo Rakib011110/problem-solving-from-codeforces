d2db5077a58fbf37cc85d28c19643ce9b24f0bf2
Rakibul Islam <rakib011110@gmail.com>
2025-01-20 19:41:58 +0600

    all code complete

--- problem14_281A.c
+++ problem14_281A.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main() {
-    char s[1001];
+    char s[101];
     scanf("%s", s);
 
+    int upper_count = 0, lower_count = 0;
 
-    if (s[0] >= 'a' && s[0] <= 'z') {
-        s[0] = s[0] - ('a' - 'A');
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (isupper(s[i])) {
+            upper_count++;
+        } else {
+            lower_count++;
+        }
     }
 
+    if (upper_count > lower_count) {
+        for (int i = 0; s[i] != '\0'; i++) {
+            s[i] = toupper(s[i]);
+        }
+    } else {
+        for (int i = 0; s[i] != '\0'; i++) {
+            s[i] = tolower(s[i]);
+        }
+    }
 
     printf("%s\n", s);
     return 0;
 }
-
