be00564e17214d339f2316793b2c405c808e2c8b
Rakibul Islam <rakib011110@gmail.com>
2025-01-20 11:21:55 +0600

    p4-231A, p5-282A

--- problem4_231A.c
+++ problem4_231A.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int n, X = 0;
+    char statement[4];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%s", statement);
+        if (statement[1] == '+') {
+            X++;
+        } else {
+            X--;
+        }
+    }
+    printf("%d\n", X);
+
+    return 0;
+}
